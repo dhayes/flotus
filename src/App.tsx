95d37416d6aace2046011805c5e40a5d2110a916
user <you@example.com>
2025-05-28 10:11:12 +0200

    basic node temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Node from './Node'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +9,9 @@ function App() {
 
   return (
     <>
+        <Node name="node1" label="Node 1" description="This is the first node." />
       <div>
+        <Node name="node1" label="Node 1" description="This is the first node." />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- src/Node.tsx
+++ src/Node.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 interface NodeProps {
     name: string;
@@ -8,10 +16,16 @@ interface NodeProps {
 
 const Node: React.FC<NodeProps> = (props: NodeProps) => {
     return (
-        <div>
-            <h2>{props.label}</h2>
-            <p>{props.description}</p>
-        </div>
+
+    <Card className="bg-[#53585a] overflow-hidden rounded-lg !gap-0 !py-1 shadow-lg">
+        <CardHeader className="bg-[#3b3f42] text-left px-4 text-black py-1 font-semibold">
+            {props.label}
+        </CardHeader>
+      <CardContent className="p-4 bg-[#696f72]">
+        This is the main card content. You can add text, buttons, etc.
+      </CardContent>
+    </Card>
+
     );
 };
 
