7b37a1582dc35309eabcaf0a01ccca8a2b70815e
user <you@example.com>
2025-05-28 18:20:46 +0200

    test add number node

---
+++ src/AddNumbers.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef } from 'react';
+import {
+    Card,
+    CardContent,
+    CardHeader,
+} from "@/components/ui/card";
+import Draggable from 'react-draggable';
+
+interface AddNumbersProps {
+    name: string;
+    label: string;
+    description?: string;
+    width?: number;
+}
+
+const AddNumbers: React.FC<AddNumbersProps> = ({ name, label, description, width }) => {
+
+    const addNumbers = (a: number, b: number): number => {
+        return a + b;
+    }
+
+    const [input1, setInput1] = React.useState<number>(0);
+    const [input2, setInput2] = React.useState<number>(0);
+    const [output, setOutput] = React.useState<number>(addNumbers(input1, input2));
+
+    useEffect(() => {
+        setOutput(addNumbers(input1, input2));
+    }, [input1, input2]);
+
+    // Create a ref for the draggable node, which is necessary for react-draggable to function correctly   
+    // Todo: switch to different draggable library that doesn't require a ref
+    const nodeRef = React.useRef<any>(null);
+
+    return (
+        <Draggable nodeRef={nodeRef}>
+            <div ref={nodeRef}>
+                <Card 
+                    className="bg-[#53585a] overflow-hidden rounded-lg !gap-0 !py-0 !shadow-none !border-none"
+                    style={width ? { width: `${width}px` } : { width: '200px' }}
+                >
+                    <CardHeader className="bg-[#3b3f42] text-left px-4 text-black !gap-0 !py-1 text-sm font-semibold font-mono select-none !shadow-none">
+                        {label}
+                    </CardHeader>
+                    <CardContent className="p-4 bg-[#696f72]">
+                        <div className='flex flex-col items-stretch gap-4 text-justify' >
+                            <div className='self-start'>
+                                <input 
+                                    className='w-1/3 py-0 px-2 bg-white text-black rounded'
+                                    type='number' 
+                                    value={input1} 
+                                    onChange={e => setInput1(Number(e.target.value))} 
+                                />
+                            </div>
+                            <div className='self-start'>
+                                <input 
+                                    className='w-1/3 py-0 px-2 bg-white text-black rounded'
+                                    type='number' 
+                                    value={input2} 
+                                    onChange={e => setInput2(Number(e.target.value))} 
+                                />
+                            </div>
+                            <div className='self-end text-right'>
+                                <input
+                                    className='w-1/3 py-0 px-2 bg-white text-black rounded'
+                                    type='text'
+                                    value={output}
+                                    readOnly
+                                /> 
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        </Draggable>
+    );
+};
+
+export default AddNumbers;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Node from './Node'
 import './App.css'
 import Stage from './Stage'
+import AddNumbers from './AddNumbers'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Stage>
         <Node name="node1" label="Node 1" description="This is the first node." />
         <Node name="node2" label="Node 2" description="This is the second node." />
+        <AddNumbers name="addNumbers" label="Add Numbers" description="This node adds two numbers." width={200} />
     </Stage>
   )
 }
