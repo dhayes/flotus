dde52a44a21b4230b5d39e992c539c851359671d
user <you@example.com>
2025-05-28 11:04:58 +0200

    basic drag functionality

--- src/Node.tsx
+++ src/Node.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
     Card,
     CardContent,
-    CardDescription,
-    CardFooter,
     CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+import Draggable from 'react-draggable';
 
 interface NodeProps {
     name: string;
@@ -14,18 +12,24 @@ interface NodeProps {
     description?: string;
 }
 
-const Node: React.FC<NodeProps> = (props: NodeProps) => {
+const Node: React.FC<NodeProps> = ({ label }) => {
+    // Create a ref for the draggable node, which is necessary for react-draggable to function correctly   
+    // Todo: switch to different draggable library that doesn't require a ref
+    const nodeRef = React.useRef<any>(null);
+
     return (
-
-    <Card className="bg-[#53585a] overflow-hidden rounded-lg !gap-0 !py-1 shadow-lg">
-        <CardHeader className="bg-[#3b3f42] text-left px-4 text-black py-1 font-semibold">
-            {props.label}
-        </CardHeader>
-      <CardContent className="p-4 bg-[#696f72]">
-        This is the main card content. You can add text, buttons, etc.
-      </CardContent>
-    </Card>
-
+        <Draggable nodeRef={nodeRef}>
+            <div ref={nodeRef}>
+                <Card className="bg-[#53585a] overflow-hidden rounded-lg !gap-0 !py-1 shadow-lg">
+                    <CardHeader className="bg-[#3b3f42] text-left px-4 text-black py-1 font-semibold select-none">
+                        {label}
+                    </CardHeader>
+                    <CardContent className="p-4 bg-[#696f72]">
+                        This is the main card content. You can add text, buttons, etc.
+                    </CardContent>
+                </Card>
+            </div>
+        </Draggable>
     );
 };
 
